151194c4d788949a14c3923205876f0ebec18790
Anthony Dellinger <anthonydellinger@outlook.com>
2015-07-03 18:26:48 -0400

    maybe now it works with azure

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
-var path = require("path");
-var express = require("express");
-var app = express();
+module.exports = function() {
+  var path = require("path");
+  var express = require("express");
+  var app = express();
 
-app.use(express.static("./static_files"));
+  app.use(express.static("./static_files"));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.resolve(process.cwd(), "./static_files/graphingcalc.html"));
-});
+  app.get('/', function (req, res) {
+    res.sendFile(path.resolve(process.cwd(), "./static_files/graphingcalc.html"));
+  });
 
-var server = app.listen(3000, function () {
+  var server = app.listen(3000, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 
-});
+  });
+};
