2d3213a7f02075ed2ccaddb3c5ee24a0c36aa6e7
Anthony Dellinger <anthonydellinger@outlook.com>
2015-07-03 16:12:25 -0400

    server working

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var path = require("path");
+var express = require("express");
+var app = express();
+
+app.use(express.static("./static_files"));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(process.cwd(), "./static_files/graphingcalc.html"));
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
