b8665452b9659df899dfd18c49bc72526f9414aa
Anthony Dellinger <anthonydellinger@outlook.com>
2015-07-03 22:42:11 -0400

    do i need a shebang?

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+"use strict";
+
 module.exports = function() {
   var path = require("path");
   var express = require("express");
@@ -6,7 +9,7 @@ module.exports = function() {
   app.use(express.static("./static_files"));
 
   app.get('/', function (req, res) {
-    res.sendFile(path.resolve(process.cwd(), "./static_files/graphingcalc.html"));
+    res.sendFile(path.resolve(__dirname, "../static_files/graphingcalc.html"));
   });
 
   var server = app.listen(3000, function () {
